8313f2b86f1cedb59fccac89a083e83bdd44e1c2
vinicius miguel <viniciusmiguel2018@hotmail.com>
2023-04-18 18:24:32 -0300

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const cors = require('cors')
 const router = require('./router')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(router);
 
 //app.get('/', (request, response) => response.status(200).send('Olá, mundo!'));
